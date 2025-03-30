fc2f0d2bf8a9f214105475ba7eb789db54110e40
joao-erik2077 <joaoerik2074@gmail.com>
2025-03-30 19:42:42 -0300

    (main): repositories

---
+++ src/main/java/com/joik2077/bootcamp/repositories/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.joik2077.bootcamp.repositories;
+
+import com.joik2077.bootcamp.models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, UUID> {
+}
