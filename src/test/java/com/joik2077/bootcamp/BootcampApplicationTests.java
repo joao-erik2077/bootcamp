07c452527a603c001728a50e0ebf3277b0114ce4
joao-erik2077 <joaoerik2074@gmail.com>
2025-03-30 19:16:43 -0300

    Initial commit

---
+++ src/test/java/com/joik2077/bootcamp/BootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joik2077.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
